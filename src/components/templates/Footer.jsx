e2bbf660211c6d25f06be7df64c8e96a087c5623
rstubryan <restubumirr@gmail.com>
2024-05-12 20:25:01 +0700

    style(footer): fix justify logo on footer

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
       <div className="flex h-[500px] flex-col justify-center overflow-x-hidden overflow-y-hidden bg-green-500 lg:h-[326px] lg:flex-row ">
         <section className="align-items-center container my-20 flex flex-col px-6 lg:flex-row lg:space-x-10 lg:px-0">
-          <div className="mb-5 w-full lg:mb-0">
+          <div className="mb-5 grid w-full items-center justify-center lg:mb-0">
             <img src={footer} alt="" />
           </div>
           <div className="mb-5 text-center lg:mb-0 lg:w-1/2 lg:text-start">
