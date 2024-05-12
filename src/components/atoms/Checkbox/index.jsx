f42b7686d48ecdde616e5dad901d92e072fd3b51
rstubryan <restubumirr@gmail.com>
2024-05-12 16:51:55 +0700

    refactor(checkbox): merging component with text

--- src/components/atoms/Checkbox/index.jsx
+++ src/components/atoms/Checkbox/index.jsx
@@ -1,11 +1,20 @@
 import { Checkbox } from "@material-tailwind/react";
+import BaseTypography from "../Typography/index.jsx";
 
-const BaseCheckbox = (props) => {
-  const { children } = props;
-
+const BaseCheckbox = () => {
   return (
     <>
-      <Checkbox label={children} />
+      <Checkbox
+        label={
+          <>
+            <BaseTypography
+              variant="small"
+              label="Saya menyetujui Syarat dan Ketentuan dan Kebijakan Privasi yang berlaku"
+              className="font-light opacity-50"
+            />
+          </>
+        }
+      />
     </>
   );
 };
