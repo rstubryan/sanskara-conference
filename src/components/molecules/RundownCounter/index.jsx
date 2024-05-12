acababecb8238a5a210f018fb40f21c99291121e
rstubryan <restubumirr@gmail.com>
2024-05-12 18:24:26 +0700

    refactor(rundown): destructuring to more readable code

--- src/components/molecules/RundownCounter/index.jsx
+++ src/components/molecules/RundownCounter/index.jsx
@@ -4,35 +4,65 @@ const RundownCounter = ({ location, nation, dd, month, yyyy }) => {
   return (
     <>
       <section>
-        <BaseTypography
-          variant="h5"
-          color="white"
-          label={location + ", " + nation}
-          className="text-base font-normal lg:text-xl"
-        />
+        <LocationEvent location={location} nation={nation} />
         <article className="flex items-center justify-center lg:justify-between">
-          <BaseTypography
-            color="white"
-            label={dd}
-            className="font-normal lg:text-7xl lg:font-semibold"
-          />
+          <DayEvent dd={dd} />
           <div className="flex items-center justify-center lg:flex-col">
-            <BaseTypography
-              variant="h4"
-              color="white"
-              className="text-base font-normal leading-tight lg:text-2xl"
-              label={<>&nbsp;{month}&nbsp;</>}
-            />
-            <BaseTypography
-              variant="h4"
-              color="white"
-              className="text-base font-normal leading-tight lg:text-2xl"
-              label={<>{yyyy}</>}
-            />
+            <MonthEvent month={month} />
+            <YearEvent yyyy={yyyy} />
           </div>
         </article>
       </section>
     </>
+  );
+};
+
+const LocationEvent = ({
+  location,
+  nation,
+  variant = "h5",
+  color = "white",
+}) => {
+  return (
+    <BaseTypography
+      variant={variant}
+      color={color}
+      label={location + ", " + nation}
+      className="text-base font-normal lg:text-xl"
+    />
+  );
+};
+
+const DayEvent = ({ dd, variant = "h1", color = "white" }) => {
+  return (
+    <BaseTypography
+      variant={variant}
+      color={color}
+      label={dd}
+      className="font-normal lg:text-7xl lg:font-semibold"
+    />
+  );
+};
+
+const MonthEvent = ({ month, variant = "h4", color = "white" }) => {
+  return (
+    <BaseTypography
+      variant={variant}
+      color={color}
+      label={<>&nbsp;{month}&nbsp;</>}
+      className="text-base font-normal leading-tight lg:text-2xl"
+    />
+  );
+};
+
+const YearEvent = ({ yyyy, variant = "h4", color = "white" }) => {
+  return (
+    <BaseTypography
+      variant={variant}
+      color={color}
+      className="text-base font-normal leading-tight lg:text-2xl"
+      label={<>{yyyy}</>}
+    />
   );
 };
 
